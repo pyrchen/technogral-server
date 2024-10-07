b3162c252d3f30bd8c3bd49741c8c56d1d032d6b
Pavel Yurchenko <pavel.yurchenko02@gmail.com>
2024-10-07 19:56:20 +0300

    routes listing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory, repl } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ILayer } from 'express-serve-static-core';
+import { TNullable } from './types/advanced.types';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -8,6 +10,21 @@ async function bootstrap() {
 
 	await repl(AppModule);
 	await app.listen(configService.get<number>('APP_PORT') || 3000);
+
+	const server = app.getHttpServer();
+	const router = server._events.request._router;
+
+	const availableRoutes: [] = router.stack
+		.map((layer: ILayer) => {
+			if (layer.route) {
+				const path = layer.route?.path;
+				const method = layer.route?.stack[0].method;
+				return `${method.toUpperCase()} ${path}`;
+			}
+		})
+		.filter((item: TNullable<string>) => item !== undefined);
+
+	console.log(availableRoutes);
 }
 
 bootstrap();
