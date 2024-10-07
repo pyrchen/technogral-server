dea3812907c6cf85b35a0b207eee9048ca1be7bc
Pavel Yurchenko <pavel.yurchenko02@gmail.com>
2024-10-07 19:48:02 +0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory, repl } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	const configService: ConfigService = app.get(ConfigService);
 
 	await repl(AppModule);
-	await app.listen(3000);
+	await app.listen(configService.get<number>('APP_PORT') || 3000);
 }
+
 bootstrap();
